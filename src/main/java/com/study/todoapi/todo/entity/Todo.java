ea181d5416e36b2d6456b2e3682af1e85ffefa8e
SeungHanLee <swq3064@naver.com>
2024-01-17 11:10:05 +0900

    # 초기 커밋

---
+++ src/main/java/com/study/todoapi/todo/entity/Todo.java
@@ -0,0 +1,35 @@
+package com.study.todoapi.todo.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_todo")
+public class Todo {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "todo_id")
+    private String id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    private boolean done;
+
+    @CreationTimestamp
+    private LocalDateTime createDate;
+
+}
